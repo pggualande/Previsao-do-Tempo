c07e3bef2289e514e544d4b59318a4169a48ca31
pggualande <paolagualande12@gmail.com>
2026-04-23 14:27:55 -0300

    Projeto finalizado

--- src/components/WeatherInfo5Days/WeatherInfo5Days.jsx
+++ src/components/WeatherInfo5Days/WeatherInfo5Days.jsx
@@ -16,19 +16,21 @@ function WeatherInfo5Days({weather5Days}){
     
     const convertDate=(date)=>{
         const newDate= new Date(date.dt * 1000).toLocaleDateString('pt-BR',{weekday:'long', day:'2-digit'})
-
+        return newDate
     }
     return(
         <div className='weather-container'>
-            <p> 5 Days</p>
+            <h3>Previsão Próximos 5 Dias</h3>
+            <div className='weather-list'>
             {next5DaysForecast.map(forecast=>(
-                <div key={forecast.dt}>
-                    <p>{convertDate(forecast)}</p>
+                <div key={forecast.dt} className='weather-item'>
+                    <p className='forecast-day'>{convertDate(forecast)}</p>
                     <img src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}/>
-                    <p>{forecast.weather[0].description}</p>
+                    <p className='forecast-description'>{forecast.weather[0].description}</p>
                     <p>{Math.round (forecast.main.temp_min)} ºC min /{Math.round(forecast.main.temp_max)} ºC máx </p>
                 </div>
             ))}
+            </div>
         </div>
     )
 }
