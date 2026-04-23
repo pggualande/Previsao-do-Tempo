475ef17b6c5c8c9a60cc8f178c9cd989c050fb71
pggualande <paolagualande12@gmail.com>
2026-04-22 22:56:48 -0300

    Previsao 5 dias completo

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import axios from 'axios'
 import heroImg from './assets/hero.png'
 import './App.css'
 import WeatherInfo from './components/WeatherInfo/WeatherInfo'
+import WeatherInfo5Days from './components/WeatherInfo5Days/WeatherInfo5Days'
 
 function App() {
   const [weather, setWeather] = useState()
+  const [weather5Days, setWeather5Days] = useState()
   const inputRef = useRef()
 
 async function searchCity(){
@@ -14,17 +16,23 @@ async function searchCity(){
   const key = "6015ad522da978933816e8c2f88e51ec"
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+  const url5Days = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&lang=pt_br&units=metric`
 
   const apiInfo = await axios.get(url)
+
+  const apiInfo5Days = await axios.get(url5Days)
+
+  setWeather5Days(apiInfo5Days.data)
   setWeather(apiInfo.data)
 }
 
   return(
-    <div>
+    <div className='container'>
       <h1>Previsão do Tempo</h1>
       <input ref={inputRef} type='text' placeholder='Digite o nome da cidade'/>
       <button onClick={searchCity}>Buscar</button>
       {weather && <WeatherInfo weather={weather}/>}
+      {weather5Days && <WeatherInfo5Days weather5Days={weather5Days} />}
     </div>
   )
 
--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -1,17 +1,25 @@
+import './WeatherInfo.css'
+
 function WeatherInfo({weather}){
     console.log(weather)
     return(
-        <div>
+        <div className='weather-container'>
             <h2>{weather.name}</h2>
-            <div>
-                <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}/>
-                <p>{Math.round(weather.main.temp)}ºC</p>
-        </div>
-        <p>{}</p>
+
+            <div className='weather-info'>
+                <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} />
+                <p className='temperature'>{Math.round(weather.main.temp)}ºC</p>
+            </div>
+
+            <p className='description'>{weather.weather[0].description}</p>
+
+            <div className='details'>
+                <p>Sensação térmica: {Math.round(weather.main.feels_like)}ºC</p>
+                <p>Umidade: {weather.main.humidity}%</p>
+                <p>Pressão: {weather.main.pressure} hPa</p>
             </div>
         </div>
     )
-
 }
 
 export default WeatherInfo
